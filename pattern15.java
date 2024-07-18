795a2d5f01d1a66d1cb71a1f1957a6e89782ee7b
RYUGAA <sakshamchauhan003@gmail.com>
2024-07-18 11:58:30 -0700

    Pattern 16

--- pattern15.java
+++ pattern15.java
@@ -3,19 +3,19 @@ public class pattern15 {
         int n= 5;
         for(int i=n ; i>=1 ;i--){
             for (int k=1;k<=(n-i); k++){
-                System.out.print("     ");
+                System.out.print("    ");
             }
             for (int j=1;j<=i;j++){
-                System.out.print("*  ");
+                System.out.print("* ");
             }
             System.out.println();
         }
         for(int i=2;i<=n;i++){
             for(int k=1; k<=(n-i);k++){
-                System.out.print("     ");
+                System.out.print("    ");
             }
             for(int j=1;j<=i;j++){
-                System.out.print("*  ");
+                System.out.print("* ");
             }
             System.out.println();
         }
