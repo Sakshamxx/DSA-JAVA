9e0fac41fac952e05b14580bffddb6c528cdd823
RYUGAA <sakshamchauhan003@gmail.com>
2024-07-25 11:55:22 -0700

    Pattern 33

--- pattern33.java
+++ pattern33.java
@@ -1,19 +1,16 @@
 public class pattern33 {
     public static void main(String[] args) {
-        int n = 10; // Number of rows
-
-        for (int i = 1; i <= n-1; i++) {
-            // Print leading spaces
-            for (int k = 0; k < n - i; k++) {
-                System.out.print("\t");
+        int m=10;
+        for (int i = 1; i <=m; i++) {
+            for(int k=1;k<=m-i;k++){
+                System.out.print("  ");
             }
-            // Print numbers
-            for (int j = 1; j <= i ; j++) {
-                if (j == i) {
-                    System.out.print( "\t");
-                } else {
-                    System.out.print((n-i+j)+"\t");
-                }
+            for (int j = 1; j <=i; j++) {
+                if(i==j) System.out.print(0+" ");
+                else System.out.print(m-i+j+" ");
+            }
+            for (int j = 2; j <=i; j++) {
+                System.out.print(m-j+1+" ");
             }
             System.out.println();
         }
